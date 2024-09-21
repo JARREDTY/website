1274d934d4a0b417e33653a9556f3b9a3bb3e153
Tyronne <tyronnenel4@gmail.com>
2024-09-22 00:58:33 +0200

    Add files via upload

---
+++ blogs/blog.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('consent', 'default', {
+    'ad_storage': 'denied',
+    'analytics_storage': 'denied'
+});
